f4724619e3fd18a0575ab061ada3c17c2753f1d8
abhishek1494k <abhi.jaiswal1494@gmail.com>
2022-12-15 16:10:32 +0530

    Media Update

--- landingPage2.js
+++ landingPage2.js
@@ -1,27 +1,24 @@
-let container=document.querySelector(".container_2");
-
-async function fetchData() {
+let url = "https://63986336044fa481d69b935b.mockapi.io/movie";
+let bag=[];
+async function getdata(){
     try {
-      let res = await fetch(
-        "https://63986336044fa481d69b935b.mockapi.io/movie"
-      );
-      let data = await res.json();
-      console.log(data)
-      renderData(data);
-    } catch (err) {
-      console.log("ERR");
+        let res= await fetch(url);
+        let out=await res.json();
+        bag=out;
+        display(out);
+    } catch (error) {
+        alert("error")
     }
-  }
+};
+getdata();
 
-
-  function renderData(data){
-    let arr=data.map((e)=>{
-        return `<div class="card">
-        <img src=${e.poster}/>
-        </div>`
-    })
-
-    container.innerHTML=arr.join("")
-  }
-
-  fetchData();
+function display(out){
+    document.querySelector(".container_2").innerHTML="";
+    out.forEach((elem) => {
+        let di= document.createElement("div");
+        let im=document.createElement("img");
+        im.setAttribute("src",elem.poster);
+        di.append(im);
+        document.querySelector(".container_2").append(di);
+    });
+}
