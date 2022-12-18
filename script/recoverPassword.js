3a0c121dc4850a9210ec36b3037b5dc854e17f15
unknown <ajitkumarkhatua286@gmail.com>
2022-12-18 14:05:27 +0530

    Ajit Day-5

--- script/recoverPassword.js
+++ script/recoverPassword.js
@@ -1,4 +1,24 @@
 
+let tvid=document.getElementById("mytvid");
+let password=document.getElementById("password");
+let captcha=document.getElementById("captchainput");
+let submitBtn=document.getElementById("submitbtn");
+
+let userdata=JSON.parse(localStorage.getItem("data"))||[]
+
+submitBtn.addEventListener("click",function(){
+    let obj={}
+    if(captcha.value==="qGphJ" ||captcha.value==="B1A4" ||captcha.value==="fdamc"){
+        obj["em"]=tvid.value;
+        obj["pa"]=password.value;
+        userdata.push(obj)
+        localStorage.setItem("data",JSON.stringify(userdata))
+        alert("Password Changed Sucessfully.")
+        window.location.href="log-in.html"
+    }else{
+        alert("Please Enter Valid Captcha.")
+    }
+})
 
 
 let newcaptcha=document.getElementById("newcaptcha")
@@ -7,13 +27,13 @@ let count=0
 newcaptcha.addEventListener("click",function(){
    
     if(count==0){
-        image.src="captcha1.png" 
+        image.src="./media/captcha1.png" 
         count++
     }else if(count==1){
-        image.src="captcha2.png"
+        image.src="./media/captcha2.png"
         count++
     }else if(count==2){
-        image.src="captcha3.png"
+        image.src="./media/captcha3.png"
         count=0
     }
 })
