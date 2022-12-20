36690dcafc2fddef577ad2818086d3f3347c0aa7
abhishek1494k <abhi.jaiswal1494@gmail.com>
2022-12-20 22:09:58 +0530

    Final Submission

--- script/sign-up.js
+++ script/sign-up.js
@@ -7,7 +7,8 @@ async function registerfun(event){
             ["name"] : all_input_tags[0].value,
             ["email"] : all_input_tags[1].value,
             ["password"] : all_input_tags[2].value,
-            ["mobile"] : all_input_tags[3].value
+            ["mobile"] : all_input_tags[3].value,
+            // ["image"] : ""
         };
         let register_req = await fetch("https://639889b9044fa481d6a154f8.mockapi.io/user",{
             method:"POST",
